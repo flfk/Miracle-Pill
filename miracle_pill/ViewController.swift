85273635acdea053cdcdf3795b04366d4a07f39a
flfk <flfk@users.noreply.github.com>
2017-03-13 15:58:14 +1100

    added UI Picker

--- miracle_pill/ViewController.swift
+++ miracle_pill/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["ACT","SA","NSW","NT","QLD","VIC"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +26,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false //i.e.unhide the statepicker
+        
+    }
 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // This means that we want one column (i.e. states)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count //how ever many componenets are in the states. thats how many rows we want
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row] //return the relevant row in the state picker
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal) //changes the button to whatever is selected in the picker
+        statePicker.isHidden = true //re-hide the state picker once selected
+    }
+    
 }
 
